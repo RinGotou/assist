40b72b9755e69386f78a4468176a9fc741388f51
RinGotou <dandeliondimn@gmail.com>
2020-01-07 20:10:23 +0800

    Add missing declaration

--- test/windows/dllmain.cpp
+++ test/windows/dllmain.cpp
@@ -4,8 +4,7 @@
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
-                     )
-{
+                     ) {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
